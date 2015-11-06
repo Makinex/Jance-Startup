cdc2a86e0ee1f6fe702a6bafd6ab6852f3686f63
Makinex <chapinhon5@hotmail.com>
2015-11-06 21:17:33 -0200

    Create consulte.php

---
+++ consulte.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+       <?php
+       include'menu.php';
+       ?>
+        <h1>Em Construção...</h1>
+    </body>
+</html>
