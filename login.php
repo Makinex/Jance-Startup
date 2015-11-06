6a7656ae9cb397d9c7d98cc3c836279fa0cffcd8
Makinex <chapinhon5@hotmail.com>
2015-11-06 21:27:04 -0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Jance</title>
+        </head>
+        <body>
+            <form action="valid-login.php" method="post">
+            <a href="cadastro-usu.php">Cadastrar</a>
+           Usuario: <input type="text" name="usuario">
+           Senha: <input type="password" name="senha">
+            <input type="submit" value="logar" name="enviar">
+            <a href="login-adm.php">Logar como ADM</a>
+            </form>
+        </body>
+    </html>
